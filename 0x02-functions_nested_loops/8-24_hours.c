df87aca09652fb44fb1e553730aba52e749606b0
blessedness77 <blessingunity01@gmail.com>
2023-02-22 11:54:33 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day
+ * Return: ...
+ */
+
+void jack_bauer(void)
+{
+	int hrs, mins;
+
+	for (hrs = 0; hrs <= 23; hrs++)
+	{
+		for (mins = 0; mins <= 59; mins++)
+		{
+			_putchar('0' + (hrs / 10));
+			_putchar('0' + (hrs % 10));
+			_putchar(':');
+			_putchar('0' + (mins / 10));
+			_putchar('0' + (mins % 10));
+			_putchar('\n');
+		}
+	}
+}
