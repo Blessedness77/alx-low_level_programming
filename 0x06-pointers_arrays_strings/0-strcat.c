4316f95f7428aaa0560e01cb3d1c1820810e9765
blessedness77 <blessingunity01@gmail.com>
2023-03-01 12:16:56 -0800

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio>
+
+/**
+ * _strcat - A function that Concatenates two string
+ * @dest: The first parameter
+ * @src: The second parameter
+ *
+ * Return: pointer to string dest.
+ */
+
+char *_strcat(char *dest, char *src);
+{
+	int conc = 0, conc_str = 0;
+
+	while (dest[conc++])
+	{
+		conc_str++;
+	}
+
+	for (conc = 0; src[conc]; conc++);
+
+	{
+		dest[conc_str++] = src[conc];
+	}
+
+	return (0);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+more functions about pointers, arrays, and strings
