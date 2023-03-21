0145e81710bb656dbabc694cb5b0a2156554f90e
blessedness77 <blessingunity01@gmail.com>
2023-03-20 17:43:12 -0700

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+working on Structures and Typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - definition of dog with some basic elements
+  * @name: name of the dog
+  * @age: the age of the dog
+  * @owner: who owns the dog
+  *
+  * Description: This is to give a brief description of the dog (struct dog)
+  */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
